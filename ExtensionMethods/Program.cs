3487268e5464de0d80ac26c87424f375a5d00fa2
Anjali Sangekar <99080406+AnjaliSangekar@users.noreply.github.com>
2024-07-10 18:28:54 +0530

    Add files via upload

---
+++ ExtensionMethods/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExtensionMethods
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string post = "This is supposed to be long long long long text";
+
+            var shortenedPost = post.Shorten(5);
+
+            Console.WriteLine(shortenedPost);
+
+
+            //
+
+            IEnumerable<int> numbers = new List<int>() { 1, 4, 7, 2, 3 };
+            var max = numbers.Max();
+            Console.WriteLine("Max number : " + max);
+
+
+        }
+    }
+}
---
+++ ExtensionMethods/StringExtension.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExtensionMethods
+{
+    public static class StringExtension
+    {
+        public static string Shorten(this String str, int numberofwords)
+        {
+            if(numberofwords < 0)
+            {
+                throw new ArgumentOutOfRangeException("number of words should be gerater than and equal to 0");
+            }
+
+            if(numberofwords == 0)
+                return "";
+
+            var words = str.Split(' ');
+
+            if(words.Length <= numberofwords )
+            {
+                return str;
+            }
+
+            return string.Join(" ", words.Take(numberofwords)) + "..." ;
+        }
+    }
+}
